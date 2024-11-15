45f3681b0639111659b04f7c2eef0b6d4d5979db
Hector Megias <hector.megias@enti.cat>
2024-11-15 10:04:01 +0100

    Siguiendo con el protocolo

--- client.sh
+++ client.sh
@@ -19,3 +19,14 @@ fi
 echo "4. CHECK OK - Enviando el FILE_NAME"
 FILE_NAME="dragon.txt"
 echo "FILE_NAME $FILE_NAME" | nc 127.0.0.1 $PORT
+
+DATA=`nc -l $PORT`
+
+echo "7. COMPROBANDO FILE_NAME"
+if [ "$DATA" != "OK_FILE_NAME" ]
+then
+	echo "ERROR 2: El filename se envio incorrectamente"
+	exit 2
+fi
+
+echo "8. CHEK OK - Filename recibido correctamente"
--- server.sh
+++ server.sh
@@ -12,24 +12,25 @@ DATA=`nc -l $PORT`
 if [ "$DATA" != "DMAM" ]
 then
     echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc localhost 2022
+	echo "KO_HEADER" | nc localhost $PORT
 	exit 1
 fi
 
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost 2022
+echo "OK_HEADER" | nc localhost $PORT
 
 
 DATA=`nc -l $PORT`
 
 echo "5. COMPROBANDO FILENAME"
-PREFIX=`echo "$DATA" | cut -d' ' -f1`
+PREFIX=`echo "$DATA" | cut -d ' ' -f 1`
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_FILENAME" | nc localhost 2022
-	exit 1
+	echo "KO_FILE_NAME" | nc localhost $PORT
+	exit 2
 fi
 
 echo "6. CHECK OK - Enviando OK_FILENAME"
+echo "OK_FILE_NAME" | nc localhost $PORT
