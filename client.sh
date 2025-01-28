ffad60bf30a94598f5961bfcec12e6d3b13eb33f
Hector Megias <hector.megias@enti.cat>
2025-01-28 18:27:22 +0100

    Protocolo finalizado

--- client.sh
+++ client.sh
@@ -18,14 +18,20 @@ fi
 
 echo "4. CHECK OK - Enviando el FILE_NAME"
 FILE_NAME="dragon.txt"
-echo "FILE_NAME $FILE_NAME" | nc 127.0.0.1 $PORT
+FILE_NAME_MD5=$(echo -n "$FILE_NAME" | md5sum | awk '{print $1}')
+echo "FILE_NAME $FILE_NAME $FILE_NAME_MD5" | nc 127.0.0.1 $PORT
 
 DATA=`nc -l $PORT`
 
 echo "7. COMPROBANDO FILE_NAME"
 if [ "$DATA" != "OK_FILE_NAME" ]
 then
-	echo "ERROR 2: El filename se envio incorrectamente"
+	if ["$DATA" != "OK_FILE_NAME" ]
+	then
+		echo "ERROR 2: El MD5 del nombre de archivo es incorrecto"
+	else
+		echo "ERROR 2: El filename se envió incorrectamente"
+	fi
 	exit 2
 fi
 
@@ -38,6 +44,24 @@ echo "11. COMPROBACION DATA"
 if [ "$DATA" != "OK_DATA" ]
 then
 	echo "ERROR 3: El contenido se envio incorrectamente"
+	exit 3
 fi
 
 echo "12. CHECK OK - Datos enviados correctamente"
+
+
+MD5SUM=$(md5sum client/$FILE_NAME | cut -d ' ' -f 1)
+echo "FILE_MD5 $MD5SUM" | nc localhost $PORT
+echo "13. Enviando FILE_MD5"
+
+
+DATA=$(nc -l $PORT)
+if [ "$DATA" != "OK_FILE_MD5" ]; then
+    echo "ERROR 4: La validación del hash MD5 falló"
+    exit 4
+fi
+echo "18. Validación correcta del hash MD5"
+
+
+
+
--- server.sh
+++ server.sh
@@ -6,7 +6,6 @@ PORT="2022"
 echo "Servidor de Dragon Magia Abuelita Miedo 2022"
 
 echo "0. ESCUCHAMOS"
-
 DATA=`nc -l $PORT`
 
 if [ "$DATA" != "DMAM" ]
@@ -19,17 +18,25 @@ fi
 echo "2. CHECK OK - Enviando OK_HEADER"
 echo "OK_HEADER" | nc localhost $PORT
 
-
 DATA=`nc -l $PORT`
 
 echo "5. COMPROBANDO FILENAME"
-PREFIX=`echo "$DATA" | cut -d ' ' -f 1`
-FILE_NAME=`echo "$DATA" | cut -d ' ' -f 2`
+PREFIX=$(echo "$DATA" | cut -d ' ' -f 1)
+FILE_NAME=$(echo "$DATA" | cut -d ' ' -f 2)
+RECEIVED_MD5=$(echo "$DATA" | cut -d ' ' -f 3)
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo incorrecto"
 	echo "KO_FILE_NAME" | nc localhost $PORT
+	exit 2
+fi
+
+CALCULATED_MD5=$(echo -n "$FILE_NAME" | md5sum | awk '{print $1}')
+if [ "$CALCULATED_MD5" != "$RECEIVED_MD5" ]
+then
+	echo "ERROR 2: MD5 del nombre de archivo incorrecto"
+	echo "KO_FILE_NAME_MD5" | nc localhost $PORT
 	exit 2
 fi
 
@@ -45,9 +52,34 @@ echo "10. COMPROBANDO DATA Y RESPUESTA"
 DRAGON=`cat /home/enti/projects/sistemasRafa/server/dragon.txt`
 if [ "$DATA" != "$DRAGON" ]
 then
-	echo "ERROR 3."
-	echo "KO_DATA"
+	echo "ERROR 3. Contenido incorrecto"
+	echo "KO_DATA" | nc localhost $PORT
 	exit 3
 fi
+
 echo "OK_DATA" | nc localhost $PORT
-cat server/$FILE_NAME
+
+
+echo "14. Escuchando FILE_MD5"
+DATA=$(nc -l $PORT)
+PREFIX=$(echo "$DATA" | cut -d ' ' -f 1)
+MD5_RECEIVED=$(echo "$DATA" | cut -d ' ' -f 2)
+
+if [ "$PREFIX" != "FILE_MD5" ]; then
+    echo "KO_FILE_MD5" | nc localhost $PORT
+    echo "ERROR 4: Prefijo FILE_MD5 incorrecto"
+    exit 4
+fi
+echo "15. Prefijo FILE_MD5 validado correctamente"
+
+MD5_GENERATED=$(md5sum server/$FILE_NAME | cut -d ' ' -f 1)
+
+echo "16. MD5 generado: $MD5_GENERATED, MD5 recibido: $MD5_RECEIVED"
+
+if [ "$MD5_GENERATED" != "$MD5_RECEIVED" ]; then
+    echo "KO_FILE_MD5" | nc localhost $PORT
+    echo "ERROR 5: El hash MD5 no coincide"
+    exit 5
+fi
+echo "OK_FILE_MD5" | nc localhost $PORT
+echo "17. Hash MD5 validado correctamente"
